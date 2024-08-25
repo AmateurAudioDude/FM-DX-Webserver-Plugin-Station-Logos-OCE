a19289c1b95a8997203ee41d6efe87c8aebd762b
AmateurAudioDude <168192910+AmateurAudioDude@users.noreply.github.com>
2024-08-25 23:19:31 +1000

    v1.2.3

--- StationLogosOCE.js
+++ StationLogosOCE.js
@@ -1,7 +1,7 @@
 // Plugin configuration, this is used in the administration when plugins are loaded
 var pluginConfig = {
     name: 'Station Logos OCE',
-    version: '1.2.2',
+    version: '1.2.3',
     author: 'AAD',
     frontEndPath: 'StationLogosOCE/pluginStationLogosOCE.js'
 }
